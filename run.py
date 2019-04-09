93093f8dddb95a451cc2792b536a2ec1f3c963e9
2019jrhee <2019jrhee@dwight.edu>
2019-04-09 10:41:20 -0400

    meow

--- run.py
+++ run.py
@@ -87,26 +87,26 @@ def new_post():
 	return render_template ('create_post.html', title= 'New Post', form=form)
 
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_login import LoginManager
 
 
 
-app = Flask(__name__)
-#for security
-app.config['SECRET_KEY'] = '0688991dd4efef8f189cf116e83258ef'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-#for password hash
-bcrypt = Bcrypt(app)
-#for login function for the user
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+# app = Flask(__name__)
+# #for security
+# app.config['SECRET_KEY'] = '0688991dd4efef8f189cf116e83258ef'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
+# #for password hash
+# bcrypt = Bcrypt(app)
+# #for login function for the user
+# login_manager = LoginManager(app)
+# login_manager.login_view = 'login'
+# login_manager.login_message_category = 'info'
 
-from flaskblog import routes
+# from flaskblog import routes
 
 if __name__ =='__main__':
 	app.run(debug==True)
